fd9f46964077d0216374113f384ac28d3a66398e
molokov-klim <ultrakawaii9654449192@gmail.com>
2022-07-21 17:11:25 +0300

    implementation fix

--- core_new.py
+++ core_new.py
@@ -8,6 +8,7 @@ from config import SHEET_ID, HOST, PORT, USER, PASSWORD, DB_NAME
 from telegram import send_telegram
 import requests
 
+
 # создает ресурс для работы с Google Sheets API
 def get_service():
     creds_json = os.path.dirname(
@@ -21,12 +22,16 @@ def get_service():
 def get_content_from_google_sheets(service):
     sheet = service.spreadsheets()
     content = sheet.values().batchGet(spreadsheetId=SHEET_ID, ranges=["Лист1"]).execute()
+    content = content['valueRanges'][0]['values']
+    content.pop(0)
     return content
+
 
 # парсинг курса валют с ЦБ РФ
 def get_exchange_rate():
     exchange_rate = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
     return exchange_rate['Valute']['USD']['Value']
+
 
 # операции с базой данных
 def db_operations(content, exchange_rate):
@@ -43,28 +48,31 @@ def db_operations(content, exchange_rate):
             isExist = cursor.fetchone()[0]
             if isExist:
                 notified_orders = check_dates(cursor)
+                #print("notified_orders")                            ####
+                #print(notified_orders)                              ####
+                content = convert_date(content)
 
 
 
-            #получаю гугл таблицу
-            #получаю курс валют
-            #проверяю есть ли таблица (формальная проверка, она должна быть)
-            #иду в базу, проверяю даты
+            # получаю гугл таблицу
+            # получаю курс валют
+            # проверяю есть ли таблица (формальная проверка, она должна быть)
+            # иду в базу, проверяю даты
             #   если дата устарела,то проверяю дату уведомления, если сегодня или 3333 то пропуск, если прошла, то отправляю уведомление в ТГ
             #   формирую тупл order_number, если notif_date = сегодня
-            #привожу таблицу к добавлению в бд:
+            # привожу таблицу к добавлению в бд:
             #   меняю формат даты
             #   добавляю сумму в рублях
             #   добавляю notif_date 3333 г. если нет совпадения по order_number. если сопадение есть то дата = сегодня
             #   преобразую в тюпл тюплов
-            #truncate
-            #формирую запрос для инсерта
-            #insert
+            # truncate
+            # формирую запрос для инсерта
+            # insert
             #
             #
             #
             #
-            #
+
 
 
 
@@ -82,30 +90,27 @@ def db_operations(content, exchange_rate):
             connection.close()
 
 
+# проверка дат уведомлений в БД. возвращает список заказов с сегодняшней датой уведомления
 def check_dates(cursor):
-    print('hi')
+    cursor.execute("select * from orders")
+    orders = cursor.fetchall()
+    for i in orders:
+        print(i)
 
+ # иду в базу, проверяю даты
+            #   если дата устарела,то проверяю дату уведомления, если сегодня или 3333 то пропуск, если прошла, то отправляю уведомление в ТГ
+            #   формирую тупл order_number, если notif_date = сегодня
 
+# конвертация даты
+def convert_date(content):
+    print(content)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # date_str = element[4]
+    # formatter_string = "%d.%m.%Y"
+    # datetime_object = datetime.strptime(date_str, formatter_string)
+    # if(datetime_object.date()<date.today()):
+    #      current_date = str(date.today())
+    #      element[5] = current_date
+    # element[4] = str(datetime_object.date())
+    # return element
 
